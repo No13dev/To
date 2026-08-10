a8636a3bbfba7d53e9a7c22918e49afe46c43b7a
No13dev <apmohsen13ple@gmail.com>
2026-08-10 10:50:10 +0330

    Added search animation

--- js/data.js
+++ js/data.js
@@ -1,11 +1,28 @@
 export const products = [
-{id:"virginia-gold",name:"ویرجینیا طلایی",category:"tobacco" weight:"100g",price:"400T",image:"images/virginia-gold.webp",status:"available"},
-{id:"T&T", name:"T&T",category:"filter",weight:"200عدد",price:"550T",image:"images/virginia-gray.webp",status:"available"}];
+{
+id: "virginia-gold",
+name: "ویرجینیا طلایی",
+category: "tobacco",
+weight: "100g",
+price: "400T",
+image: "images/virginia-gold.webp", 
+status: "available" 
+},
+{
+id: "tt",
+name: "T&T",
+category: "filter",
+weight: "200عدد",
+price: "550T",
+image: "images/virginia-gray.webp",
+status: "available"
+} 
+];
 export const categories = [
-{ id:"tobacco", name:"توتون"}, 
-{id:"filter",name:"پوکه"},
-{id:"cigar",name:"سیگار برگ"},
-{id:"pack",name:"سیگار"},
-{id:"accessory",name:"اکسسوری"},
-{id:"machine",name:"دستگاه پرکن"},
+{ id: "tobacco", name: "توتون"}, 
+{ id: "filter", name: "پوکه"},
+{ id: "cigar", name: "سیگار برگ"},
+{ id: "pack", name: "سیگار"},
+{ id: "accessory", name: "اکسسوری"},
+{ id: "machine", name: "دستگاه پرکن"}
 ];
